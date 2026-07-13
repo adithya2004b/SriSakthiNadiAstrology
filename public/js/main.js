d1262efc5b360b9c22d36e33cfc7a090431dae20
adithya2004b <adithya2004b@gmail.com>
2026-07-13 10:08:17 +0530

    nadi

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 // ===== Constants =====
-const CONTACT_EMAIL = 'adithya3572@gmail.com';
+const CONTACT_EMAIL = 'nadiastrologyhyderabad@gmail.com';
 
 const KANDAMS = [
   { id: 1, title: 'General Chapter', desc: 'Overview of your entire life, family, character, and brief predictions for all other chapters.' },
